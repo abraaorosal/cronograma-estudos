c4d72489b9992d0e2bf7b4a287a074f014d091be
Abraão Henrique <146304449+abraaorosal@users.noreply.github.com>
2026-09-22 20:17:15 -0300

    chore: wire OAB 48 application entrypoint

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
-import './index.css'
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import CronogramaEstudos from './App'
+import './index.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <CronogramaEstudos />
-  </React.StrictMode>
-)
+  <React.StrictMode><App /></React.StrictMode>
+);
